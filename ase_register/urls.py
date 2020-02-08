22d7eb28efd7441ee8529fc01e82de05339d9f5e
RamananRavichandran <gr.ramanan2010@gmail.com>
2020-02-08 12:41:07 +0530

    first commit

--- ase_register/urls.py
+++ ase_register/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ase_form),
-    path('list/',views.ase_list),
-    path('form/',views.ase_form)
+    path('', views.ase_form,name='ase_insert'), #get and post request for insert operation
+    path('<int:id>/', views.ase_form,name='ase_update'), #get and post request for update operation
+    path('list/',views.ase_list,name='ase_list'), #get request to retrieve and display all records
+    path('delete/<int:id>/',views.ase_delete,name='ase_delete')
 ]
--- ase_register/views.py
+++ ase_register/views.py
@@ -7,17 +7,28 @@ def ase_list(request):
     context = {'ase_list':Ase.objects.all()}
     return render(request,"ase_register/ase_list.html",context)
 
-def ase_form(request):
+def ase_form(request, id=0):
     if request.method == 'GET':
-        form = AseForm()
+        if id == 0:
+            form = AseForm()
+        else:
+            ase = Ase.objects.get(pk=id)
+            form = AseForm(instance=ase)
         return render(request,"ase_register/ase_form.html",{'form':form})
+        
     else:
-        form = AseForm(request.POST)
+        if id == 0:
+            form = AseForm(request.POST)
+        else:
+            ase = Ase.objects.get(pk=id)
+            form = AseForm(request.POST,instance=ase)
         if form.is_valid():
             form.save()
             return redirect('/ase/list')
         else:
             return redirect('/ase/form')
-            
-def ase_delete(request):
-    return 
+
+def ase_delete(request,id):
+    ase = Ase.objects.get(pk=id)
+    ase.delete()
+    return redirect('/ase/list')
