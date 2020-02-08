8bf498e8acfa5ce922a2e9fe1788241662a7fb5f
RamananRavichandran <gr.ramanan2010@gmail.com>
2020-02-08 22:06:56 +0530

    app all done without awesome icons

--- ase_register/models.py
+++ ase_register/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from phone_field import PhoneField
 
 # Create your models here.
 class Ase(models.Model):
     fullname = models.CharField(max_length=100)
-    emp_code = models.CharField(max_length=20)
-    mobile = models.IntegerField(20)
+    emp_code = models.CharField(max_length=20,unique=True)
+    mobile = PhoneField(blank=True, help_text='Contact phone number')
     dateoj = models.DateField()
